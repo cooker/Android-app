f0f7994c61b59891b8f35eb7c457af3cfac6c942
grant <fishkq@aliyun.com>
2022-06-21 17:08:30 +0800

    init

--- app/src/main/java/com/github/g9527/application/MainActivity.java
+++ app/src/main/java/com/github/g9527/application/MainActivity.java
@@ -1,14 +1,41 @@
 package com.github.g9527.application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.g9527.application.views.BillActivity;
+import com.github.g9527.application.views.ControlActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        View controlView = findViewById(R.id.main_icon_1);
+        controlView.setOnClickListener(this);
+        View billView = findViewById(R.id.main_icon_2);
+        billView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Class<?> cl = null;
+        switch (v.getId()) {
+            case R.id.main_icon_1:
+                cl = ControlActivity.class;
+                break;
+            case R.id.main_icon_2:
+                cl = BillActivity.class;
+                break;
+        }
+
+        Intent intent = new Intent(this, cl);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/github/g9527/application/views/BillActivity.java
+++ app/src/main/java/com/github/g9527/application/views/BillActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.github.g9527.application.R;
 
-public class ControlActivity extends AppCompatActivity {
+public class BillActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_control);
+        setContentView(R.layout.activity_bill);
     }
 }
